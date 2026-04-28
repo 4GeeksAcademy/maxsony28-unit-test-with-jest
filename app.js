95c2524ef32de01d7d538420984ca7c9cea3f825
Maxwel Hernandez <maxwelhernandez@gmail.com>
2026-04-28 21:02:12 +0000

    instalando jest

---
+++ app.js
@@ -0,0 +1,15 @@
+//funcion que suma 
+
+function sum(num1,num2) {
+    let result = num1 + num2
+    return result
+}
+
+console.log(sum(5, 2))
+
+function subtract(num1,num2) {
+    let result = num1 - num2 
+    return result
+}
+
+console.log(subtract(5,2))
